1eef4bfaaf029fb1ad9f8a2d1c597d41ce40b69c
Ahmed Bham <ahmed.bham@gmail.com>
2021-09-15 02:19:58 +0000

    create dapr az functions

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -29,6 +29,7 @@ cd dapr-springboot/demo-dapr-consumer
 ./mvnw spring-boot:build-image -DskipTests
 docker tag demo-dapr-consumer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/dapr-consumer
 docker push k3d-registry.localhost:5000/dapr-consumer
+docker rmi dapr-consumer:demo-dapr-consumer:0.0.1-SNAPSHOT
 cd ../../
 
 # create dapr-springboot producer app
@@ -37,6 +38,7 @@ cd dapr-springboot/demo-dapr-producer
 ./mvnw spring-boot:build-image -DskipTests
 docker tag demo-dapr-producer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/dapr-producer
 docker push k3d-registry.localhost:5000/dapr-producer
+docker rmi demo-dapr-producer:0.0.1-SNAPSHOT
 cd ../../
 
 
@@ -46,6 +48,7 @@ cd springboot-k8s/demo-consumer
 ./mvnw spring-boot:build-image -DskipTests
 docker tag demo-consumer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/springboot-consumer
 docker push k3d-registry.localhost:5000/springboot-consumer
+docker rmi demo-consumer:0.0.1-SNAPSHOT
 cd ../../
 
 
@@ -55,13 +58,26 @@ cd springboot-k8s/demo-producer
 ./mvnw spring-boot:build-image -DskipTests
 docker tag demo:0.0.1-SNAPSHOT k3d-registry.localhost:5000/springboot-producer
 docker push k3d-registry.localhost:5000/springboot-producer
+docker rmi demo:0.0.1-SNAPSHOT
 cd ../../
 
 # set up kafka for k8s cluster
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+# deploy KEDA on k8s cluster
+helm repo add kedacore https://kedacore.github.io/charts
+helm repo update
+kubectl create namespace keda
+helm install keda kedacore/keda --namespace keda
 
+helm install ingress-controller bitnami/nginx-ingress-controller \
+    --namespace ingress-nginx \
+    --set controller.replicaCount=1 \
+    --set controller.metrics.enabled=true \
+    --set controller.podAnnotations."prometheus\.io/scrape"="true" \
+    --set controller.podAnnotations."prometheus\.io/port"="10254"  \
+    --set controller.service.type=NodePort
 
 # create the cluster
 make create
